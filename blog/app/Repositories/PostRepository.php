d5e01a74425764cac716328c6463623480753b24
Vanilla666 <41668091+Vanilla666@users.noreply.github.com>
2018-11-30 14:16:10 +0800

    Update PostRepository.php

--- blog/app/Repositories/PostRepository.php
+++ blog/app/Repositories/PostRepository.php
@@ -8,7 +8,7 @@ class PostRepository //做資料庫查詢
 {
     public function index() //查詢文章與使用者資訊
     {
-        return Post::with('user')->get();  //Post與User關聯
+        return Post::with('user')->get(); //Post與User關聯 Post的user_id 為user的外來鑑
     }
 
     public function find($id) //找到第ID筆文章
